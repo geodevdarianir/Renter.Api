435f2b1ed24a7b21b5940a7d014141356440a02c
Rati Devdariani <devdarianirati1401@gmail.com>
2021-04-08 15:53:53 +0200

    UserRols

--- Entities/Models/Users/Role.cs
+++ Entities/Models/Users/Role.cs
@@ -2,10 +2,27 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Common.UserIdentityCommon;
+using System.Linq;
 
 namespace Entities.Models.Users
 {
     public class Role : IdentityRole<Guid>
     {
+        public override string Name
+        {
+            get => base.Name; set
+            {
+                var props = typeof(UserRoles).GetFields();
+                if (props.Any(p => p.Name == value))
+                {
+                    base.Name = value;
+                }
+                else
+                {
+                    throw new Exception("Role is only Admin, Client or Contractor!");
+                }
+            }
+        }
     }
 }
